8674f7e1004a69038b855f1f1401f676977995c5
Аноним Haker <Rick-Sanches2020@yandex.ru>
2024-11-05 23:10:59 +0300

    Добавление configs

---
+++ client/src/config/routerConfig.ts
@@ -0,0 +1,10 @@
+export enum RouterPathes {
+    Info = '/',
+    Author = '/author',
+    Home = '/home',
+    Login = '/auth/sing-in',
+    Register = '/auth/sing-up',
+    Error = '/error',
+    NotFound = '*',
+    Doc = '/home/documents'
+}
