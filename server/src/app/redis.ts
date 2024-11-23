9bf00eff03bb46fbc4f57769ddef943fed5f6bf2
unknown <Rick-Sanches2020@yandex.ru>
2024-11-23 23:49:59 +0300

    Переделал configs

--- server/src/app/redis.ts
+++ server/src/app/redis.ts
@@ -1,9 +1,6 @@
 import Redis from 'ioredis'
-import dotenv from 'dotenv'
+import { REDIS_PORT, REDIS_HOST } from '../config/config.server'
 
-dotenv.config({ path: 'src/.env' })
-const REDIS_PORT = Number( process.env.REDIS_PORT ) || 6379
-const REDIS_HOST = process.env.REDIS_HOST || 'localhost'
 
 export const redis = new Redis({
     port: REDIS_PORT,
--- server/src/config/config.server.ts
+++ server/src/config/config.server.ts
@@ -7,3 +7,7 @@ export const PORT: number     = Number( process.env.PORT )     || 3000
 export const HOST: string     = String( process.env.HOST )     || '127.0.0.1'
 export const NODE_ENV: string = String( process.env.NODE_ENV ) || 'development'
 export const isDev = NODE_ENV === 'development'
+
+
+export const REDIS_PORT = Number( process.env.REDIS_PORT ) || 6379
+export const REDIS_HOST = process.env.REDIS_HOST || 'localhost'
